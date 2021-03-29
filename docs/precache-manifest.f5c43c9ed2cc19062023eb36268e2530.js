260eb13ea644ec96f8961cb12f84bc6f2aaa6c05
AquilesAllelLillo <aquilesallellillo@gmail.com>
2021-03-29 14:43:53 -0300

    Carpeta docs (build) agregada

---
+++ docs/precache-manifest.f5c43c9ed2cc19062023eb36268e2530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011c93d5efd10eff7111f447c2be9099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5d69dedb2bd493d57ff",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d69dedb2bd493d57ff",
+    "url": "/static/js/main.2a47a075.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
